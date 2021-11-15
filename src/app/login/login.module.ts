fb7d37641971fffc8c6db9bdccb1cafb0a444618
dylanmeuriot <dylanmeuriot@gmail.com>
2021-11-15 15:22:36 +0100

    Commit 3 : pages

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,9 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { LoginRoutingModule } from './login-routing.module';
+import { PageSignInComponent } from './pages/page-sign-in/page-sign-in.component';
+import { PageSignUpComponent } from './pages/page-sign-up/page-sign-up.component';
+import { PageForgotPasswordComponent } from './pages/page-forgot-password/page-forgot-password.component';
+import { PageResetPasswordComponent } from './pages/page-reset-password/page-reset-password.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageSignInComponent,
+    PageSignUpComponent,
+    PageForgotPasswordComponent,
+    PageResetPasswordComponent
+  ],
   imports: [CommonModule, LoginRoutingModule],
 })
 export class LoginModule {}
