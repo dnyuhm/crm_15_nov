c059984fbf3f184e090696fd55e68ada171879c6
dylanmeuriot <dylanmeuriot@gmail.com>
2021-11-15 14:26:04 +0100

    Commit 1 : Modules

---
+++ src/app/orders/orders.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { OrdersRoutingModule } from './orders-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, OrdersRoutingModule],
+})
+export class PageNotFoundModule {}
