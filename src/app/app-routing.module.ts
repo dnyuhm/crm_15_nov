22b26cc2006909444303b54e0a0808ebc4c04587
dylanmeuriot <dylanmeuriot@gmail.com>
2021-11-16 15:13:53 +0100

    Commit 14 : router angular & lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PageListClientsComponent } from './clients/pages/page-list-clients/page-list-clients.component';
 import { PageForgotPasswordComponent } from './login/pages/page-forgot-password/page-forgot-password.component';
 import { PageResetPasswordComponent } from './login/pages/page-reset-password/page-reset-password.component';
 import { PageSignInComponent } from './login/pages/page-sign-in/page-sign-in.component';
@@ -12,6 +11,23 @@ const routes: Routes = [
   { path: 'sign-up', component: PageSignUpComponent },
   { path: 'reset', component: PageResetPasswordComponent },
   { path: 'forgot', component: PageForgotPasswordComponent },
+  {
+    path: 'orders',
+    loadChildren: () =>
+      import('./orders/orders.module').then((m) => m.OrdersModule),
+  },
+  {
+    path: 'clients',
+    loadChildren: () =>
+      import('./clients/clients.module').then((m) => m.ClientsModule),
+  },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./page-not-found/page-not-found.module').then(
+        (m) => m.PageNotFoundModule
+      ),
+  },
 ];
 
 @NgModule({
