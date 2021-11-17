8fdde7e14e36b1823a031f9f3f228bb2f4afad2f
dylanmeuriot <dylanmeuriot@gmail.com>
2021-11-17 09:23:20 +0100

    Commit 19 : Enums, interfaces & models

--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-list-orders',
   templateUrl: './page-list-orders.component.html',
-  styleUrls: ['./page-list-orders.component.scss']
+  styleUrls: ['./page-list-orders.component.scss'],
 })
 export class PageListOrdersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
