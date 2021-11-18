f61cf744e64bbf160b7600e5fbc4cde20ad3ad51
dylanmeuriot <dylanmeuriot@gmail.com>
2021-11-18 15:32:03 +0100

    Commit 29 : StateDirective & hostBinding

--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -33,7 +33,7 @@ export class PageListOrdersComponent implements OnInit {
   public changeState(item: Order, event: any): void {
     const state = event.target.value;
     this.ordersService.changeState(item, state).subscribe((data) => {
-      item = data;
+      Object.assign(item, data);
     });
   }
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,14 +5,16 @@ import { TemplatesModule } from '../templates/templates.module';
 import { TableLightComponent } from './components/table-light/table-light.component';
 import { BtnComponent } from './components/btn/btn.component';
 import { TotalPipe } from './pipes/total.pipe';
+import { StateDirective } from './directives/state.directive';
 
 @NgModule({
   declarations: [
     TableLightComponent,
     BtnComponent,
-    TotalPipe
+    TotalPipe,
+    StateDirective
   ],
   imports: [CommonModule],
-  exports: [IconsModule, TemplatesModule, TableLightComponent, BtnComponent, TotalPipe],
+  exports: [IconsModule, TemplatesModule, TableLightComponent, BtnComponent, TotalPipe, StateDirective],
 })
 export class SharedModule {}
